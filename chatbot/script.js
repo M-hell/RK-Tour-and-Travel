e2c6bc03e250c3e16f83994a196b006421801f45
Samrat Sarkar <sarkarsamrat16112004@gmail.com>
2024-02-18 12:05:23 +0530

    Update script.js

--- chatbot/script.js
+++ chatbot/script.js
@@ -5,7 +5,7 @@ const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 
 let userMessage = null; // Variable to store user's message
-const API_KEY = "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"; // Paste your API key here
+const API_KEY = ####; // Paste your API key here
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
